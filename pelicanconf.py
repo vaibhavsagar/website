e3050e5fdd0d7651484e79963512eea336c87984
vaibhavsagar <vaibhavsagar@gmail.com>
2015-04-05 00:08:55 +1100

    Remove blogroll.

--- pelicanconf.py
+++ pelicanconf.py
@@ -33,11 +33,6 @@ FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'))
-
 # Social widget
 SOCIAL = ()
 
