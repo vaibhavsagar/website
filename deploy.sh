64d8cf61f57602e7aacbd54fee04992fdda07e89
vaibhavsagar <vaibhavsagar@gmail.com>
2015-07-11 02:50:54 +1000

    Try shallow clone.

--- deploy.sh
+++ deploy.sh
@@ -9,8 +9,8 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
         git config --global user.email "travis@travis-ci.org"
         git config --global user.name "Travis"
     fi
-    # using token clone gh-pages branch
-    git clone --quiet --branch=$BRANCH https://${GH_TOKEN}@github.com/$TARGET_REPO built_website > /dev/null
+    # using token, clone gh-pages branch
+    git clone --depth 1 --quiet --branch=$BRANCH https://${GH_TOKEN}@github.com/$TARGET_REPO built_website > /dev/null
     # go into directory and copy data we're interested in to that directory
     cd built_website
     rsync -rv --delete --exclude=.git  ../$PELICAN_OUTPUT_FOLDER/* .
