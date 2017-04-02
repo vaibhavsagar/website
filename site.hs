b253084116f7da7440829a4aa407d5468bb37d27
Vaibhav Sagar <vaibhavsagar@gmail.com>
2017-04-03 00:31:49 +0700

    Clean URLs

--- site.hs
+++ site.hs
@@ -2,6 +2,8 @@
 {-# LANGUAGE OverloadedStrings #-}
 import           Data.Monoid (mappend)
 import           Hakyll
+import           System.FilePath
+import           Data.List (isSuffixOf)
 
 
 --------------------------------------------------------------------------------
@@ -16,22 +18,27 @@ main = hakyll $ do
         compile compressCssCompiler
 
     match (fromList ["about.rst", "contact.markdown"]) $ do
-        route   $ setExtension "html"
+        route     cleanRoute
         compile $ pandocCompiler
             >>= loadAndApplyTemplate "templates/default.html" defaultContext
             >>= relativizeUrls
+            >>= cleanIndexUrls
 
-    match "posts/*" $ do
-        route $ setExtension "html"
+    match "blog/*" $ do
+        route   $ cleanRoute `composeRoutes` dateRoute
         compile $ pandocCompiler
             >>= loadAndApplyTemplate "templates/post.html"    postCtx
             >>= loadAndApplyTemplate "templates/default.html" postCtx
             >>= relativizeUrls
 
-    create ["archive.html"] $ do
+    match "extra/*" $ do
+        route     rootRoute
+        compile   copyFileCompiler
+
+    create ["archive/index.html"] $ do
         route idRoute
         compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
+            posts <- recentFirst =<< loadAll "blog/*"
             let archiveCtx =
                     listField "posts" postCtx (return posts) `mappend`
                     constField "title" "Archives"            `mappend`
@@ -41,12 +48,13 @@ main = hakyll $ do
                 >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
                 >>= loadAndApplyTemplate "templates/default.html" archiveCtx
                 >>= relativizeUrls
+                >>= cleanIndexUrls
 
 
     match "index.html" $ do
         route idRoute
         compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
+            posts <- fmap (take 10) . recentFirst =<< loadAll "blog/*"
             let indexCtx =
                     listField "posts" postCtx (return posts) `mappend`
                     constField "title" "Home"                `mappend`
@@ -56,6 +64,7 @@ main = hakyll $ do
                 >>= applyAsTemplate indexCtx
                 >>= loadAndApplyTemplate "templates/default.html" indexCtx
                 >>= relativizeUrls
+                >>= cleanIndexUrls
 
     match "templates/*" $ compile templateBodyCompiler
 
@@ -63,5 +72,30 @@ main = hakyll $ do
 --------------------------------------------------------------------------------
 postCtx :: Context String
 postCtx =
-    dateField "date" "%B %e, %Y" `mappend`
+    dateField "date" "%e %B %Y" `mappend`
     defaultContext
+
+cleanRoute :: Routes
+cleanRoute = customRoute createIndexRoute
+    where createIndexRoute ident =
+            takeDirectory p </> takeBaseName p </> "index.html"
+            where p = toFilePath ident
+
+rootRoute :: Routes
+rootRoute = customRoute (takeFileName . toFilePath)
+
+dateRoute :: Routes
+dateRoute = gsubRoute yearPattern (replaceAll "-" (const "/"))
+    where yearPattern = "/[0-9]{4}-[0-9]{2}-[0-9]{2}-"
+
+cleanIndexUrls :: Item String -> Compiler (Item String)
+cleanIndexUrls = return . fmap (withUrls cleanIndex)
+
+cleanIndexHtmls :: Item String -> Compiler (Item String)
+cleanIndexHtmls = return . fmap (replaceAll "/index.html" (const "/"))
+
+cleanIndex :: String -> String
+cleanIndex url
+    | index `isSuffixOf` url = take (length url - length index) url
+    | otherwise              = url
+    where index = "index.html"
