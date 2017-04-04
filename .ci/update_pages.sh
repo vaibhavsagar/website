b3a6879dcbc43d314a148ffd73271e8672e181d7
Vaibhav Sagar <vaibhavsagar@gmail.com>
2017-04-04 17:16:45 +0700

    Use rsync to copy files over

--- .ci/update_pages.sh
+++ .ci/update_pages.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 BRANCH=gh-pages
 TARGET_REPO=vaibhavsagar/hakyll-website.git
+OUTPUT_FOLDER=_site
 
 echo -e "Starting to deploy to Github Pages\n"
 if [ "$TRAVIS" == "true" ]; then
@@ -8,10 +9,10 @@ if [ "$TRAVIS" == "true" ]; then
     git config --global user.name "Travis"
 fi
 # Using token, clone gh-pages branch
-git clone --quiet --branch=$BRANCH "https://$GH_TOKEN@github.com/$TARGET_REPO" build > /dev/null
+git clone --depth 1 --quiet --branch=$BRANCH "https://$GH_TOKEN@github.com/$TARGET_REPO" build > /dev/null
 # Go into directory and copy data we're interested in to that directory
 cd build || exit
-cp -R ../_site/* .
+rsync -av --delete --exclude=.git  ../$OUTPUT_FOLDER/ ./
 # Add, commit and push files
 git add -f .
 git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to Github Pages"
