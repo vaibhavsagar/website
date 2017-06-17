b552d230b488bd5d9a8a7abd95a923f4d9345634
Vaibhav Sagar <vaibhavsagar@gmail.com>
2017-06-17 21:13:53 +0800

    Commit to Markdown

--- site.hs
+++ site.hs
@@ -21,7 +21,7 @@ main = hakyll $ do
         route   idRoute
         compile compressCssCompiler
 
-    match (fromList ["about.rst", "contact.markdown"]) $ do
+    match (fromList ["about.md", "contact.md"]) $ do
         route     cleanRoute
         compile $ pandocCompiler
             >>= loadAndApplyTemplate "templates/default.html" defaultContext
