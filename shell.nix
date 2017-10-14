1306b9d3272de171ed956dc4cb00ab200ea13f01
Vaibhav Sagar <vaibhavsagar@gmail.com>
2017-10-14 18:06:21 +0800

    Use shell.nix for both nix-build and nix-shell

--- shell.nix
+++ shell.nix
@@ -1,6 +1,4 @@
 { nixpkgs ? import <nixpkgs> {} }:
-(nixpkgs.haskellPackages.callCabal2nix
-  "website"
-  (nixpkgs.lib.cleanSource ./.)
-  {}
-).env
+let
+  drv = nixpkgs.haskellPackages.callCabal2nix "website" (nixpkgs.lib.cleanSource ./.) {};
+in if nixpkgs.lib.inNixShell then drv.env else drv
