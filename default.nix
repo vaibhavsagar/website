4e4ccde8c789e5f894de9caa0279b6205f00c263
Vaibhav Sagar <vaibhavsagar@gmail.com>
2017-12-01 00:01:02 +0800

    Reduce rebuilds by ignoring non-Haskell source files

--- default.nix
+++ default.nix
@@ -1,4 +1,11 @@
 { nixpkgs ? import <nixpkgs> {} }:
 let
-  drv = nixpkgs.haskellPackages.callCabal2nix "website" (nixpkgs.lib.cleanSource ./.) {};
+  inherit (builtins) any filterSource;
+  lib = nixpkgs.lib;
+  sourceFilter = src: name: type: let
+    relPath = lib.removePrefix (toString src + "/") (toString name);
+  in lib.cleanSourceFilter name type && (any (lib.flip lib.hasPrefix relPath) [
+    "site.hs" "website.cabal" "LICENSE"
+  ]);
+  drv = nixpkgs.haskellPackages.callCabal2nix "website" (filterSource (sourceFilter ./.) ./.) {};
 in if nixpkgs.lib.inNixShell then drv.env else drv
