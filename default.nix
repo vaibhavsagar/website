3600f07a9fdcb3799ef07cc466db0cb3ddf82321
Vaibhav Sagar <vaibhavsagar@gmail.com>
2018-04-11 10:14:00 +0800

    default.nix: replace fetchFromGitHub with builtins.fetchTarball

--- default.nix
+++ default.nix
@@ -1,8 +1,12 @@
 let
-  inherit (import <nixpkgs> {}) fetchFromGitHub lib;
+  inherit (import <nixpkgs> {}) lib;
   # ./updater versions.json nixpkgs nixos-18.03
+  fetcher = { owner, repo, rev, sha256 }: builtins.fetchTarball {
+    inherit sha256;
+    url = "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz";
+  };
   versions = lib.mapAttrs
-    (_: fetchFromGitHub)
+    (_: fetcher)
     (builtins.fromJSON (builtins.readFile ./versions.json));
   inherit (builtins) any filterSource;
   nixpkgs = import versions.nixpkgs {};
