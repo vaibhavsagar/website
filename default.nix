797293a6a6165ae03d974706d7a3aea4dd9e2671
Vaibhav Sagar <vaibhavsagar@gmail.com>
2018-04-26 00:08:07 +0800

    default.nix: simplify

--- default.nix
+++ default.nix
@@ -1,19 +1,15 @@
 let
-  inherit (import <nixpkgs> {}) lib;
   # ./updater versions.json nixpkgs nixos-18.03
   fetcher = { owner, repo, rev, sha256 }: builtins.fetchTarball {
     inherit sha256;
     url = "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz";
   };
-  versions = lib.mapAttrs
-    (_: fetcher)
-    (builtins.fromJSON (builtins.readFile ./versions.json));
-  inherit (builtins) any filterSource;
-  nixpkgs = import versions.nixpkgs {};
+  nixpkgs = import (fetcher (builtins.fromJSON (builtins.readFile ./versions.json)).nixpkgs) {};
+  lib = nixpkgs.lib;
   sourceFilter = src: name: type: let
     relPath = lib.removePrefix (toString src + "/") (toString name);
-  in lib.cleanSourceFilter name type && (any (lib.flip lib.hasPrefix relPath) [
+  in lib.cleanSourceFilter name type && (builtins.any (lib.flip lib.hasPrefix relPath) [
     "site.hs" "website.cabal" "LICENSE"
   ]);
-  drv = nixpkgs.haskellPackages.callCabal2nix "website" (filterSource (sourceFilter ./.) ./.) {};
-in if nixpkgs.lib.inNixShell then drv.env else drv
+  drv = nixpkgs.haskellPackages.callCabal2nix "website" (builtins.filterSource (sourceFilter ./.) ./.) {};
+in if lib.inNixShell then drv.env else drv
