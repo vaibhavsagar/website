44bc7e2a0a8332adcfe46836c809741858b25736
Vaibhav Sagar <vaibhavsagar@gmail.com>
2018-07-21 20:50:16 +0800

    Simplify build process

--- .ci/update_pages.sh
+++ .ci/update_pages.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 BRANCH=master
 TARGET_REPO=vaibhavsagar/vaibhavsagar.github.io.git
-OUTPUT_FOLDER=_site
+OUTPUT_FOLDER=result
 
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-    echo -e "Starting to deploy to Github Pages\n"
+    echo -e "Starting to deploy to Github Pages\\n"
     if [ "$TRAVIS" == "true" ]; then
 	git config --global user.email "travis@travis-ci.org"
 	git config --global user.name "Travis"
@@ -13,7 +13,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     git clone --depth 1 --quiet --branch=$BRANCH "https://$GH_TOKEN@github.com/$TARGET_REPO" build > /dev/null
     # Go into directory and copy data we're interested in to that directory
     cd build || exit 1
-    rsync -av --delete --exclude=.git  ../$OUTPUT_FOLDER/ ./
+    rsync -avL --delete --exclude=.git  ../$OUTPUT_FOLDER/ ./
     # Add, commit and push files
     git add --all .
     git commit --allow-empty -m "Travis build $TRAVIS_BUILD_NUMBER pushed to Github Pages"
--- default.nix
+++ default.nix
@@ -6,10 +6,24 @@ let
   };
   nixpkgs = import (fetcher (builtins.fromJSON (builtins.readFile ./versions.json)).nixpkgs) {};
   lib = nixpkgs.lib;
-  sourceFilter = src: name: type: let
-    relPath = lib.removePrefix (toString src + "/") (toString name);
-  in lib.cleanSourceFilter name type && (builtins.any (lib.flip lib.hasPrefix relPath) [
+  relPath = src: name: lib.removePrefix (toString src + "/") (toString name);
+  sourceFilter = src: name: type: lib.cleanSourceFilter name type && (builtins.any (lib.flip lib.hasPrefix (relPath src name)) [
     "site.hs" "website.cabal" "LICENSE"
   ]);
+  contentFilter = src: name: type: lib.cleanSourceFilter name type && (builtins.any (lib.flip lib.hasPrefix (relPath src name)) [
+    "blog" "css" "drafts" "extra" "index.html" "pages" "templates"
+  ]);
   drv = nixpkgs.haskellPackages.callCabal2nix "website" (builtins.filterSource (sourceFilter ./.) ./.) {};
-in if lib.inNixShell then drv.env else drv
+  site = nixpkgs.runCommand "site" {
+    buildInputs = [ drv nixpkgs.glibcLocales ];
+    src = builtins.filterSource (contentFilter ./.) ./.;
+    LC_ALL = "en_US.UTF-8";
+  } ''
+    workdir=$(${nixpkgs.coreutils}/bin/mktemp -d)
+    cp -R $src/* $workdir
+    cd $workdir
+    site build
+    mkdir -p $out
+    cp -R _site/* $out
+  '';
+in if lib.inNixShell then drv.env else site
