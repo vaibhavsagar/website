417fcb5f2302e8ace2a9e249e87a80db5cc062cc
Vaibhav Sagar <vaibhavsagar@gmail.com>
2019-10-14 21:49:40 -0400

    Use NixOS 19.09

--- default.nix
+++ default.nix
@@ -4,13 +4,19 @@ let
     inherit sha256;
     url = "https://github.com/${owner}/${repo}/tarball/${rev}";
   };
-  nixpkgs = import (fetcher (builtins.fromJSON (builtins.readFile ./versions.json)).nixpkgs) {};
+  nixpkgs = import (fetcher (builtins.fromJSON (builtins.readFile ./versions.json)).nixpkgs) { config.allowBroken = true; };
   lib = nixpkgs.lib;
   myFilter = ls: src: name: type: let
     relPath = lib.removePrefix (toString src + "/") (toString name);
   in lib.cleanSourceFilter name type && (builtins.any (lib.flip lib.hasPrefix relPath) ls);
   contentFilter = myFilter [ "blog" "css" "drafts" "extra" "index.html" "pages" "templates" ];
-  drv = nixpkgs.haskellPackages.callPackage ./website.nix {};
+  overrides = self: super: {
+    hakyll = nixpkgs.haskell.lib.overrideCabal super.hakyll {
+      patches = [];
+      doCheck = false;
+    };
+  };
+  drv = (nixpkgs.haskellPackages.override { inherit overrides; }).callPackage ./website.nix {};
   site = nixpkgs.runCommand "site" {
     buildInputs = drv.env.nativeBuildInputs;
     src = builtins.filterSource (contentFilter ./.) ./.;
