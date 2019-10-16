9c66426048beee3bfbdd5f800115155bd7998387
Vaibhav Sagar <vaibhavsagar@gmail.com>
2019-10-15 22:49:06 -0400

    versions.json: bump to fix broken Hakyll

--- default.nix
+++ default.nix
@@ -4,19 +4,13 @@ let
     inherit sha256;
     url = "https://github.com/${owner}/${repo}/tarball/${rev}";
   };
-  nixpkgs = import (fetcher (builtins.fromJSON (builtins.readFile ./versions.json)).nixpkgs) { config.allowBroken = true; };
+  nixpkgs = import (fetcher (builtins.fromJSON (builtins.readFile ./versions.json)).nixpkgs) {};
   lib = nixpkgs.lib;
   myFilter = ls: src: name: type: let
     relPath = lib.removePrefix (toString src + "/") (toString name);
   in lib.cleanSourceFilter name type && (builtins.any (lib.flip lib.hasPrefix relPath) ls);
   contentFilter = myFilter [ "blog" "css" "drafts" "extra" "index.html" "pages" "templates" ];
-  overrides = self: super: {
-    hakyll = nixpkgs.haskell.lib.overrideCabal super.hakyll {
-      patches = [];
-      doCheck = false;
-    };
-  };
-  drv = (nixpkgs.haskellPackages.override { inherit overrides; }).callPackage ./website.nix {};
+  drv = nixpkgs.haskellPackages.callPackage ./website.nix {};
   site = nixpkgs.runCommand "site" {
     buildInputs = drv.env.nativeBuildInputs;
     src = builtins.filterSource (contentFilter ./.) ./.;
