d9956ba1d967dc1aab75d7dbe3ea8ac24c4cc915
Vaibhav Sagar <vaibhavsagar@gmail.com>
2024-02-03 17:16:48 +1100

    flake.nix: update

--- flake.nix
+++ flake.nix
@@ -1,22 +1,22 @@
 {
-  inputs.nixpkgs-src.url = "github:NixOS/nixpkgs/release-23.11";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-23.11";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nix-filter.url = "github:numtide/nix-filter";
   inputs.flake-compat.url = "github:edolstra/flake-compat";
-  outputs = {nixpkgs-src, flake-utils, nix-filter, ...}:
+  outputs = {nixpkgs, flake-utils, nix-filter, ...}:
     flake-utils.lib.eachDefaultSystem (system: let
-      nixpkgs = import nixpkgs-src { inherit system; };
+      pkgs = import nixpkgs { inherit system; };
       content = nix-filter {
         root = ./.;
         include = [ "blog" "css" "drafts" "extra" "index.html" "pages" "templates" ];
       };
-      drv = nixpkgs.haskellPackages.callPackage ./website.nix {};
-      site = nixpkgs.runCommand "site" {
+      drv = pkgs.haskellPackages.callPackage ./website.nix {};
+      site = pkgs.runCommand "site" {
         buildInputs = drv.env.nativeBuildInputs;
         src = content;
         LANG = "C.UTF-8";
       } ''
-        workdir=$(${nixpkgs.coreutils}/bin/mktemp -d)
+        workdir=$(${pkgs.coreutils}/bin/mktemp -d)
         cp -R $src/* $workdir
         cd $workdir
         mkdir -p $out
