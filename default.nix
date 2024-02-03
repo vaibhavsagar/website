760a4b4c8f44347fbe3bb39203383c4a1b42178d
Vaibhav Sagar <vaibhavsagar@gmail.com>
2024-02-03 17:07:27 +1100

    flake.nix: init

--- default.nix
+++ default.nix
@@ -1,26 +1,10 @@
-let
-  # ./updater versions.json nixpkgs nixos-18.03
-  fetcher = { owner, repo, rev, sha256, ... }: builtins.fetchTarball {
-    inherit sha256;
-    url = "https://github.com/${owner}/${repo}/tarball/${rev}";
-  };
-  nixpkgs = import (fetcher (builtins.fromJSON (builtins.readFile ./versions.json)).nixpkgs) {};
-  lib = nixpkgs.lib;
-  myFilter = ls: src: name: type: let
-    relPath = lib.removePrefix (toString src + "/") (toString name);
-  in lib.cleanSourceFilter name type && (builtins.any (lib.flip lib.hasPrefix relPath) ls);
-  contentFilter = myFilter [ "blog" "css" "drafts" "extra" "index.html" "pages" "templates" ];
-  drv = nixpkgs.haskellPackages.callPackage ./website.nix {};
-  site = nixpkgs.runCommand "site" {
-    buildInputs = drv.env.nativeBuildInputs;
-    src = builtins.filterSource (contentFilter ./.) ./.;
-    LANG = "C.UTF-8";
-  } ''
-    workdir=$(${nixpkgs.coreutils}/bin/mktemp -d)
-    cp -R $src/* $workdir
-    cd $workdir
-    mkdir -p $out
-    ln -sfn $out _site
-    runhaskell ${./site.hs} build
-  '';
-in site
+(import
+  (
+    let lock = builtins.fromJSON (builtins.readFile ./flake.lock); in
+    fetchTarball {
+      url = lock.nodes.flake-compat.locked.url or "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+      sha256 = lock.nodes.flake-compat.locked.narHash;
+    }
+  )
+  { src = ./.; }
+).defaultNix
--- website.nix
+++ website.nix
@@ -1,10 +1,16 @@
-{ mkDerivation, base, blaze-html, filepath, hakyll, ghc-syntax-highlighter, pandoc, text, lib }:
+{ mkDerivation, base, blaze-html, filepath, ghc-syntax-highlighter
+, hakyll, lib, pandoc, pandoc-types, text
+}:
 mkDerivation {
   pname = "website";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base blaze-html filepath hakyll ghc-syntax-highlighter pandoc text ];
+  executableHaskellDepends = [
+    base blaze-html filepath ghc-syntax-highlighter hakyll pandoc
+    pandoc-types text
+  ];
   license = lib.licenses.bsd3;
+  mainProgram = "site";
 }
